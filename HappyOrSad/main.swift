07adc10a180e2889bfc6e8a2d5eb42d82f7d3254
Russell Gordon <rgordon@lcs-mba2410.local>
2018-04-04 09:35:39 -0400

    Initial Commit

---
+++ HappyOrSad/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HappyOrSad
+//
+//  Created by Gordon, Russell on 2018-04-04.
+//  Copyright © 2018 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
